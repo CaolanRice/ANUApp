20db95ef2c00ea322b7d76fb47777c5781f2525f
CaolanRice <c_f_rice@hotmail.co.uk>
2023-03-13 16:46:25 +0100

    Initial commit

---
+++ src/main/java/com/caolan/Main.java
@@ -0,0 +1,31 @@
+package com.caolan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public greetingResponse greeting(){
+        greetingResponse response = new greetingResponse(
+                "Hello",
+                List.of("Java", "JavaScript", "Python"),
+                new Person("Caolan", 29, 400.00));
+
+        return response;
+    }
+
+    record Person(String name, int age, double moneys){ }
+
+    record greetingResponse(String greet, List<String> langs, Person person){ }
+
+}
